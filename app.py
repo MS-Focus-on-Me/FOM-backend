f6da4603722a7039d80a8d44f95ee3f8678f83a5
Zoonhyeong <wkcpq35@naver.com>
2025-05-21 17:05:31 +0900

    CORS

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # CORS 허용 정책 추가
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # 모든 출처 허용, 특정 출처만 허용하려면 리스트에 URL 넣기
+    allow_origins=["*"],  # 모든 출처 허용, 특정 출처만 허용하려면 리스트에 URL 넣기
     allow_credentials=True,
     allow_methods=["*"],    # 모든 HTTP 메소드 허용
     allow_headers=["*"],    # 모든 헤더 허용
