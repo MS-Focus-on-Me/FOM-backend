d857005c83cd7999b77437103c1aef63ed8e74e5
Zoonhyeong <wkcpq35@naver.com>
2025-05-21 11:56:40 +0900

    login 구현

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -16,5 +17,18 @@ app.add_middleware(
 async def read_root():
     return {"message": "Hello, FastAPI"}
 
-# test
-# 이제 어떡하죠
+@app.post("/name")
+async def name():
+    return {"message": "Hello, FastAPI"}
+
+class UserLogin(BaseModel):
+    email: str
+    password: str
+
+@app.post("/api/login")
+async def login(user: UserLogin):
+    print(user)  # 데이터 잘 도착했는지 출력 확인
+    if user.email == "test@test.com" and user.password == "123":
+        return {"success": True, "message": "로그인 성공!"}
+    else:
+        return {"success": False, "message": "로그인 실패: 이메일 또는 비밀번호가 잘못되었습니다."}
