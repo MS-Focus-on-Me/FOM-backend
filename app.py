52bcdaebaba017b73a5cc5a98fba305dfdac3d80
Zoonhyeong <wkcpq35@naver.com>
2025-05-30 17:13:37 +0900

    python-version downgrade

--- app.py
+++ app.py
@@ -224,3 +224,7 @@ async def read_root():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app:app", host="0.0.0.0", port=port)
+
+
+# source venv/bin/activate     
+# uvicorn app:app --host 0.0.0.0 --port 8000
